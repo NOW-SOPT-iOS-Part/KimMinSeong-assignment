ccf7a432a262309c637f7f21279b909dfd530a2a
minKim-dev <lovejd0815@gmail.com>
2024-04-30 23:09:39 +0900

    [FEAT] Home화면 UI구현(compositional layout으로 구현)

--- Week3-Assignment-Tving-Home/Week3-Assignment-Tving-Home/View/HomeView.swift
+++ Week3-Assignment-Tving-Home/Week3-Assignment-Tving-Home/View/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  MainView.swift
+//  HomeView.swift
 //  Week3-Assignment-Tving-Home
 //
 //  Created by 김민성 on 4/30/24.
@@ -9,6 +9,186 @@ import UIKit
 import Then
 import SnapKit
 
-class MainView() {
-    let collcetionView: 
+final class HomeView: UIView {
+    // MARK: UIComponents
+    // 필요한 UIComponents -> Collection View, LogoView, TopTabBar
+    lazy var collectionView: UICollectionView = { // property initializer가 self가 돌아간 이후에 available 하므로 lazy로 선언해야함.
+        // layout 잡아줘야함 함수로 처리하고 이름은 makeCollectionViewLayout()로 하자.
+        let layout = makeCollectionViewLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.isScrollEnabled = true
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        
+        return collectionView
+    }()
+    
+    private let logoView = LogoView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        backgroundColor = .black
+    }
+    
+    private func setHierarchy() {
+        addSubview(collectionView)
+        collectionView.addSubview(logoView)
+        
+    }
+    
+    private func setLayout() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        logoView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+
+    }
+    
 }
+
+// MARK: extensions
+
+private extension HomeView {
+    
+    // MARK: CollectionView Layout
+    
+    private func makeCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionNumber, env) -> NSCollectionLayoutSection? in
+            
+            if sectionNumber == 0 { // MainCollection View Cell에 해당
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)))
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(498)), subitems: [item]) // group이 가로로 존재
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .paging
+                
+                return section
+                
+            } else if sectionNumber == 1 { // recommandCollectionViewCell에 해당
+                
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)))
+                item.contentInsets = .init(top: 0, leading: 8, bottom: 0, trailing: 8) // 아이템 사이의 간격
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.3), heightDimension: .absolute(146)), subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPaging
+                
+                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50))
+                
+                let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+                section.boundarySupplementaryItems = [header]
+                
+                return section
+                
+            } else if sectionNumber == 2 { // popularLiveChannelCollectionView에 해당
+                
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)))
+                item.contentInsets = .init(top: 0, leading: 7, bottom: 0, trailing: 7) // 아이템 사이의 간격
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.48), heightDimension: .absolute(80)), subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .paging
+                
+                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50))
+                
+                let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+                section.boundarySupplementaryItems = [header]
+                
+                return section
+                
+            } else {
+                let item = NSCollectionLayoutItem(
+                layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+                item.contentInsets.bottom = 44
+                                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(498)), subitems: [item])
+                                
+                let section = NSCollectionLayoutSection(group: group)
+                                
+                section.orthogonalScrollingBehavior = .paging
+                                
+                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(100))
+                let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+                section.boundarySupplementaryItems = [header]
+                                
+                return section
+            }
+        }
+    }
+    
+    // MARK: CollectionView Layout 함수 분리
+    
+//    private func makeCompositionalLayout() {
+//        let layout = UICollectionViewCompositionalLayout { (sectionIndex,_) -> NSCollectionLayoutSection? in
+//            return self.createSection(for: sectionIndex)
+//        }
+//    }
+//    
+//    private func createSection(for sectionIndex: Int) -> NSCollectionLayoutSection {
+//        switch sectionIndex {
+//            case 0:
+//                return createMainCollectionViewSection()
+//            case 1:
+//                return createSubHorizontalScrollingSection()
+//            default:
+//                return createHorizontalScrollingSection()
+//        }
+//    }
+//    
+//    private func createHorizontalScrollingSection() -> NSCollectionLayoutSection {
+//        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.7))
+//        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//        
+//        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.3))
+//        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+//        
+//        let section = NSCollectionLayoutSection(group: group)
+//        
+//        return section
+//    }
+//    
+//    private func createMainHorizontalScrollingSection() -> NSCollectionLayoutSection {
+//        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+//        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//        
+//        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(4.0), heightDimension: .fractionalHeight(1.0))
+//        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+//        
+//        let section = NSCollectionLayoutSection(group: group)
+//        
+//        return section
+//    }
+//    
+//    private func createSubHorizontalScrollingSection() -> NSCollectionLayoutSection {
+//        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(1.0))
+//        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//        item.contentInsets = NSDirectionalEdgeInsets(top: 80, leading: 8, bottom: 0, trailing: 8)
+//        
+//        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.5), heightDimension: .fractionalHeight(1.0))
+//        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+//        
+//        let section = NSCollectionLayoutSection(group: group)
+//        
+//        return section
+//    }
+}
+
