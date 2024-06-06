857d3288aba4e7739e5366de0b6d4eea216bd439
minKim-dev <lovejd0815@gmail.com>
2024-06-07 04:53:14 +0900

    [Fix] #10 - 네트워크 통신 오류 해결

--- Week2-Assignment-Tving/Sopt-Assignment/Network/PatchMovieInfo/MovieInfoResponseModel.swift
+++ Week2-Assignment-Tving/Sopt-Assignment/Network/PatchMovieInfo/MovieInfoResponseModel.swift
@@ -7,14 +7,27 @@
 
 import Foundation
 
-struct MovieInfoResponseModel: Codable {
-    let data: DataClass
+// MARK: - DailyBoxOffice
+
+struct DailyBoxOffice: Codable {
+    let boxOfficeResult: BoxOfficeResult
 }
 
-struct DataClass: Codable {
-    let code: Int /// 응답 코드
-    let rank: String /// 해당 일자의 박스오피스 순위를 의미
-    let movieNm: String /// 영화명(국문)을 의미
-    let salesAcc: String /// 누적 매출액을 의미
-    let audiAcc: String /// 누적 관객수를 의미
+// MARK: - BoxOfficeResult
+
+struct BoxOfficeResult: Codable {
+    let boxofficeType, showRange: String
+    let dailyBoxOfficeList: [DailyBoxOfficeList]
+}
+
+// MARK: - DailyBoxOfficeList
+
+struct DailyBoxOfficeList: Codable {
+    let rank, title, audienceNumber: String
+    
+    enum CodingKeys: String, CodingKey {
+        case rank
+        case title = "movieNm"
+        case audienceNumber = "audiAcc"
+    }
 }
